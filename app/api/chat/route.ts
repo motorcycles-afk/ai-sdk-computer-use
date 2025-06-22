31997fc91fc7fb5e99360667526a607ed740440f
motorcycles-afk <catttaa347@gmail.com>
2025-06-22 17:45:26 +0200

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -12,13 +12,7 @@ export async function POST(req: Request) {
     await req.json();
   try {
     const result = streamText({
-      model: anthropic("claude-3-7-sonnet-20250219"), // Using Sonnet for computer use
-      system:
-        "You are a helpful assistant with access to a computer. " +
-        "Use the computer tool to help the user with their requests. " +
-        "Use the bash tool to execute commands on the computer. You can create files and folders using the bash tool. Always prefer the bash tool where it is viable for the task. " +
-        "Be sure to advise the user when waiting is necessary. " +
-        "If the browser opens with a setup wizard, YOU MUST IGNORE IT and move straight to the next step (e.g. input the url in the search bar).",
+      model: anthropic("claude-3-7-sonnet-20250219"),
       messages: prunedMessages(messages),
       tools: { computer: computerTool(sandboxId), bash: bashTool(sandboxId) },
       providerOptions: {
@@ -26,7 +20,6 @@ export async function POST(req: Request) {
       },
     });
 
-    // Create response stream
     const response = result.toDataStreamResponse({
       // @ts-expect-error eheljfe
       getErrorMessage(error) {
